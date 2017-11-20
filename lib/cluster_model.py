493225dafd445da04973249de6a579b175fce07a
Pinreeeeeeen <pc2751@columbia.edu>
2017-11-19 23:25:03 -0500

    cluster_model function

---
+++ lib/cluster_model.py
@@ -0,0 +1,98 @@
+import pandas as pd
+import numpy as np
+import pandas as pd
+
+
+
+
+
+X=np.load("C:/Users/zjutc/Desktop/train.out.npy")
+
+
+def train_cluster_model(X,k):
+    m,n=X.shape
+    h=np.unique(X)
+    """
+    use EM algorithm to train multinomial mixture model
+    X: user matrix with Xi for each row Xi=[vi1,vi2,...,vin]
+    m: users
+    n: items
+    k: clusters
+    h: labels for votes. It should be a numeric vector starting from 0-X
+    PI: assignment matrix
+    c: weights for cluster
+    
+    THETA : three dimensional array for all parameters (kxnxh) 
+    THETA[k]: k th cluster parameters. It's a nxh matrix. Item in n th row and d th column means the probability of n th vote being d
+    In THETA[k], sum of each row should be 1 
+    
+    """
+    def likelihood(X,k,THETA):
+        """
+        This is used to calculate likelihood function P(Xi|thetaK)
+        Xi means ith user. It is a vector: (Vi1,Vi2,...Vin). It records ith user's votes for n items. e.g (1,2,5,6,0,2,...1)
+        k means kth cluster. The parameters for kth cluster is THETA[k]
+        THETA: three dimensional array for all parameters as mentioned above.
+        """
+        # return probability multiplication of all users' votes in one cluster and return the vector
+        # vector=(user1P,user2P,...)
+        
+        
+        theta_k=THETA[k]
+        theta_selected=theta_k[np.arange(theta_k.shape[0]),X]
+        return theta_selected.prod(axis=1)
+    # set initial values for cluster weights
+    c=np.repeat(1.0/k,k)
+    
+    # randomly assign parameters for each cluster
+    THETA=[]
+    for i in range(k):
+        randoms=np.random.random(n*len(h))
+        theta_matrix=randoms.reshape(n,len(h)) # theta_matrix: THETA[K] nxh matrix
+        theta_matrix=theta_matrix/theta_matrix.sum(axis=1).reshape(n,1) # for each item the sum of h should be 1
+        THETA.append(theta_matrix)
+    
+    # create assign matrix A:
+    A=np.zeros((m,k))
+    
+    log_likelihood=0
+    log_likelihood_old=100
+    while np.absolute(log_likelihood-log_likelihood_old)>0.001:
+        
+        # E-step:
+        log_likelihood_old=log_likelihood
+        
+        for i in range(k):
+            A[:,i]=likelihood(X,i,THETA)*c[i]
+        log_likelihood=np.sum(np.log(A.sum(axis=1)))
+        print log_likelihood
+        print log_likelihood_old
+        ###
+        #for row in range(X.shape[0]):
+        #    log_likelihood_Xi=0
+        #    for i in range(k): # i th cluster
+        #        p=likelihood(X[row],i,THETA)*c[i]
+        #        A[row,i]=p
+        #        log_likelihood_Xi+=p
+        #    log_likelihood+=np.log(log_likelihood_Xi)
+        A=A/A.sum(axis=1).reshape(m,1)
+        #print log_likelihood
+        #print log_likelihood_old
+        
+        # M-step:
+        
+        # 1. recompute weights for cluster
+        c=A.sum(axis=0)/m
+        #print c
+        
+        # 2. reassign parameters for each cluster
+        for i in range(k):
+            A_selected=A[:,i]    
+            v1=(X==0).T.dot(A_selected)/A_selected.sum(axis=0)
+            v2=(X==1).T.dot(A_selected)/A_selected.sum(axis=0)
+            THETA[i][:,0]=v1
+            THETA[i][:,1]=v2
+        
+    return [THETA,A,c]
+        
+THETA,A,c=train_cluster_model(X,3)
