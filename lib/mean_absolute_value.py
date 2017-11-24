b40b2fd009b51509488bcb184e9cd1f97d65667b
Fan Gong <gongfan1993@gmail.com>
2017-11-23 22:03:47 -0500

    mean_absolute_value function

---
+++ lib/mean_absolute_value.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 23 20:04:51 2017
+
+@author: Fan
+"""
+
+
+def mean_absolute_error(test_df, predict_df):
+    '''
+    This function aims to calculate the mean absolute error of our prediction
+    
+    Parameter
+    ---------
+    test_df : A pandas dataframe that contains all the information about the test 
+    active users and their voting. 
+    predict_df : A pandas dataframe that contians all the prediction information
+    
+    Return
+    ------
+    MAE : A number represents the mean absolute error
+    '''
+    
+    import pandas as pd
+    import numpy as np
+    
+    # Preprocess the data
+    test_df = test_df.replace(0, np.nan) # replace 0 with na
+    test_df = test_df.dropna(axis = [0,1], how = 'all') # delete the rows and columns that are all NAs
+    
+    m,n = test_df.shape # m is the number of the test user, and n is the number of 
+    MAE = 0 # initial mean absolute value
+    
+    # For each of the user, we need to calculate the absolute difference 
+    for i in np.arange(m):
+        
+        user = test_df.index[i] # user name
+        item = test_df.loc[user, :].dropna() # item list 
+        item_name = item.index # voted item name
+        
+        predict_item = predict_df.loc[user, list(item_name)] # predict voted value
+        
+        MAE = MAE + np.abs(predict_item - item).sum()/len(item) # calculate the sum of MAE
+        
+    
+    MAE = MAE/m # Calculate the mean of absolute value
+        
+        
+    return MAE
+         
+
+
