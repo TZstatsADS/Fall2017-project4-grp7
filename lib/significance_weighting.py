945de44c4b6ac5b8e868bc7bb49a9078fddca835
Pinren Chen <pc2751@columbia.edu>
2017-11-28 19:26:17 -0500

    redesign significance.py

--- lib/significance_weighting.py
+++ lib/significance_weighting.py
@@ -11,26 +11,31 @@ import pandas as pd
 import pickle
 
 ###############################data1
-train1 = pd.read_csv('./output/train1_df.csv',index_col = 0)
+
+def main():
+    train1 = pd.read_csv('./output/train1_df.csv',index_col = 0)
 
 
-with open('./PearsonCosineMatrix/pearson_correlation1.pkl', 'rb') as f1:
-    pearson_corr1 = pickle.load(f1)
-pearson_corr1 = pearson_corr1.values
+    with open('./PearsonCosineMatrix/pearson_correlation1.pkl', 'rb') as f1:
+        pearson_corr1 = pickle.load(f1)
+    pearson_corr1 = pearson_corr1.values
 
 ###############################data2
-train2 = pd.read_csv('./output/train2_df.csv',index_col = 0)
+    train2 = pd.read_csv('./output/train2_df.csv',index_col = 0)
 
 
-with open('./PearsonCosineMatrix/pearson_correlation2.pkl', 'rb') as f2:
-    pearson_corr2 = pickle.load(f2)
-pearson_corr2 = pearson_corr2.values
+    with open('./PearsonCosineMatrix/pearson_correlation2.pkl', 'rb') as f2:
+        pearson_corr2 = pickle.load(f2)
+    pearson_corr2 = pearson_corr2.values
 
+    sig_weights1,devalued_weights1=significance_weighting(train1)
+    sig_weights2,devalued_weights2=significance_weighting(train2)
 
 ###########################significance weighting 
 #<50 common rated items, significance weight = n/50
 #>50 common rated items, significance weight = 1
-def significance_weighting(data):
+def significance_weighting(data,correlation):
+    data=data.values
     n_users = data.shape[0]
     co_rated = np.zeros((n_users, n_users))
     sig_weight = np.zeros((n_users, n_users))
@@ -41,10 +46,11 @@ def significance_weighting(data):
                  sig_weight[i,j] = 1
              else:
                  sig_weight[i,j] = co_rated[i,j]/50
-    return sig_weight
+    devalued_weight=correlation*sig_weight
+    return [sig_weight,devalued_weight]
 
-sig_weights1 = significance_weighting(train1.values)
-devalued_weights1 = pearson_corr1*sig_weights1 
-sig_weights2 = significance_weighting(train2.values)
-devalued_weights2 = pearson_corr2*sig_weights2
+
  
+ 
+if __name__ == '__main__':
+    main()
