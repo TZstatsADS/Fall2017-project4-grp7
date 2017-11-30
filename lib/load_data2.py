e524df8fc524f22c95c1ac5f1924d11e124a9bad
Pinren Chen <pc2751@columbia.edu>
2017-11-30 00:00:51 -0500

    final commit

--- lib/load_data2.py
+++ lib/load_data2.py
@@ -10,8 +10,7 @@ Created on Thu Nov 23 15:42:33 2017
 import pandas as pd
 import numpy as np
 
-
-def load_data2(each_movie_data):
+def load_data2(train_data,test_data):
 
 
     all_user = np.union1d(train_data["User"],test_data["User"])
@@ -22,11 +21,15 @@ def load_data2(each_movie_data):
     train_data_matrix = np.zeros((n_users, n_items))
     for line in train_data.itertuples():
         train_data_matrix[np.where(all_user==line[2])[0]-1, np.where(all_movie == line[1])[0]-1] = line[3]
-
-
-    train2_df = pd.DataFrame(train_data_matrix, index = all_user, columns = all_movie)
     
-    return train2_df
+    test_data_matrix = np.zeros((n_users, n_items))
+    for line in test_data.itertuples():
+        test_data_matrix[np.where(all_user==line[2])[0]-1, np.where(all_movie == line[1])[0]-1] = line[3]
+    
+    train2_df = pd.DataFrame(train_data_matrix, index = all_user, columns = all_movie)
+    test2_df = pd.DataFrame(test_data_matrix, index = all_user, columns = all_movie)
+    
+    return (train2_df,test2_df)
     
 
 
